f5504656fbe85f3778dd8ef804070c780dec728f
Sam Lucas <smlucas13@gmail.com>
2015-09-05 12:00:05 -0400

    save

--- flappy-phaser/game.js
+++ flappy-phaser/game.js
@@ -1,1 +1,22 @@
 console.log("WIP");
+
+'use strict';
+
+var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+  {preload:preload,create:create,update:update,render:render});
+var sea
+
+function preload() {
+  game.load.image("sea" "sea.gif");
+}
+
+function create() {
+  backround = game.add
+}
+
+function update() {
+}
+
+function render() {
+}
+
