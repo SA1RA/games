2666775c0a9224c4005e580372cb28042480936c
Sam Lucas <smlucas13@gmail.com>
2015-09-19 09:47:41 -0400

    save

--- flappy-phaser/game.js
+++ flappy-phaser/game.js
@@ -1,17 +1,23 @@
-console.log("WIP");
-
 'use strict';
 
 var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
-var sea
+
+var backround;
+var ship;
 
 function preload() {
-  game.load.image("sea" "sea.gif");
+  game.load.image("backround","assets/backround.gif");
+  game.load.image("ship","assets/ship.png");
 }
 
 function create() {
-  backround = game.add
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  background = game.add.tileSprite(0,0,320,568,"backround");
+  background.autoScroll(-100,0);
+  ship = game.add.sprite(10,284,"ship");
+  game.physics.enable(ship, Phaser.Physics.ARCADE);
+  flappy.body.gravity.y = 1000;
 }
 
 function update() {
