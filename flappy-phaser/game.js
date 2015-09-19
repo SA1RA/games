14a6eafdd006e78f0f2a663a22c65603efdb7dd7
Sam Lucas <smlucas13@gmail.com>
2015-09-19 10:40:50 -0400

    save

--- flappy-phaser/game.js
+++ flappy-phaser/game.js
@@ -1,26 +1,32 @@
 'use strict';
 
-var game = new Phaser.Game(800,600,Phaser.AUTO,'game',
+var game = new Phaser.Game(320,568,Phaser.AUTO,'game',
   {preload:preload,create:create,update:update,render:render});
 
-var backround;
+var background;
 var ship;
+var jumpTimer = 0
 
 function preload() {
-  game.load.image("backround","assets/backround.gif");
-  game.load.image("ship","assets/ship.png");
+  game.stage.backgroundColor = '#bbbbbb';
+  game.load.image("background","background.png");
+  game.load.image("ship","ship.png");
 }
 
 function create() {
   game.physics.startSystem(Phaser.Physics.ARCADE);
-  background = game.add.tileSprite(0,0,320,568,"backround");
+  background = game.add.tileSprite(0,0,320,568,"background");
   background.autoScroll(-100,0);
   ship = game.add.sprite(10,284,"ship");
   game.physics.enable(ship, Phaser.Physics.ARCADE);
-  flappy.body.gravity.y = 1000;
+  ship.body.gravity.y = 100;
 }
 
 function update() {
+  if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+  ship.body.velocity.y = -100;
+  jumpTimer = game.time.now + 750;
+  }
 }
 
 function render() {
